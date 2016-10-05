396872e344cb28e42db1c69d8b5684c6ae1f2d8b
corey <832hallogallo832>
2016-10-05 14:10:47 -0500

    Can now see Gutenberg result for URL (restored pre-Angular functionality)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,14 @@
  */
 angular.module('markovmutatorApp')
   .controller('MainCtrl', function ($scope, $http) {
+    $scope.books = [];
     $scope.addBook = function() {
       var url = JSON.stringify(document.querySelector("#url").value);
       $http.post('/addBook', url)
       .then(function successCallback(response) {
+        $scope.books.push(response.data)
+        console.log("Success");
+        console.log(response);
         // this callback will be called asynchronously
         // when the response is available
       }, function errorCallback(response) {
